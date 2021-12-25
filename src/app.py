81444ba10a230124f1b6189e561820d300934c03
Tom Hüttmann <github@huettmann.me>
2021-12-25 13:39:02 +0100

    add car propterties to calculate overall distance

--- src/app.py
+++ src/app.py
@@ -19,15 +19,18 @@ def base():
 @app.route('/car/<car_id>')
 def car(car_id):
     if car_id in car_ids:
+        car_properties = get_car_properties_from(car_id)
         fuel_consumptions = add_price_to(add_average_consumption_to(get_chronological_fuel_consumptions_from(car_id)))
-        total_average_consumption = round(100 * sum(map(lambda x: x['amount'], fuel_consumptions)) / sum(
-            map(lambda x: x['distance'], fuel_consumptions)), 2)
+        total_amount = sum(map(lambda x: x['amount'], fuel_consumptions))
+        total_distance = sum(map(lambda x: x['distance'], fuel_consumptions))
+        total_average_consumption = round(100 * total_amount / total_distance, 2)
         # noinspection PyUnresolvedReferences
         return render_template(
             'car.html',
-            car_id=car_id,
+            car_name=car_properties["name"],
             fuel_consumptions=fuel_consumptions,
-            total_average_consumption=total_average_consumption
+            total_average_consumption=total_average_consumption,
+            overall_distance=f"{round(total_distance + car_properties['base_distance']):,}".replace(",", ".")
         )
     else:
         return "car not found"
@@ -38,6 +41,13 @@ def get_chronological_fuel_consumptions_from(car_id):
     fuel_consumptions = json.load(file)
     file.close()
     return sorted(fuel_consumptions, key=lambda x: datetime.strptime(x['date'], '%d.%m.%Y'), reverse=True)
+
+
+def get_car_properties_from(car_id):
+    file = open('data/' + car_id + '/car_properties.json')
+    car_properties = json.load(file)
+    file.close()
+    return car_properties
 
 
 def add_average_consumption_to(fuel_consumptions):
