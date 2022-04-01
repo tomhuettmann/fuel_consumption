276bfdd72ad4610040131753cd28a026b1aea46f
Tom Hüttmann <github@huettmann.me>
2022-04-01 22:29:40 +0200

    feat: start with the migration to GitHub pages

--- src/app.py
+++ src/app.py
@@ -2,41 +2,43 @@ import json
 import os
 from datetime import datetime, timedelta
 
-from flask import Flask, render_template
+from jinja2 import Environment, FileSystemLoader
 
-app = Flask(__name__)
+root = os.path.dirname(os.path.abspath(__file__))
+templates_dir = os.path.join(root, 'templates')
+env = Environment(loader=FileSystemLoader(templates_dir))
+index_template = env.get_template('index.html')
+car_template = env.get_template('car.html')
 
 
-@app.route('/')
-def base():
-    # noinspection PyUnresolvedReferences
-    return render_template(
-        'index.html',
-        car_ids=sorted(car_ids)
+def generate_index():
+    filename = os.path.join(root, '../docs', 'index.html')
+    with open(filename, 'w') as fh:
+        fh.write(index_template.render(
+            car_ids=sorted(car_ids)
+        ))
+
+
+def generate_car(car_id):
+    car_properties = get_car_properties_from(car_id)
+    fuel_consumptions = add_total_price_to(
+        add_average_consumption_to(get_chronological_fuel_consumptions_from(car_id)))
+    total_amount = sum(map(lambda x: x['amount'], fuel_consumptions))
+    total_distance = sum(map(lambda x: x['distance'], fuel_consumptions))
+    total_average_consumption = round(100 * total_amount / total_distance, 2)
+    fuel_consumptions_last_year, last_year_days = get_all_fuel_consumptions_and_days_within_last(365,
+                                                                                                 fuel_consumptions)
+    distance_per_year = 365 * sum(
+        map(lambda x: x['distance'], list(fuel_consumptions_last_year)[:-1])) / last_year_days
+    fuel_consumptions_last_three_months, last_three_months_days = get_all_fuel_consumptions_and_days_within_last(
+        90, fuel_consumptions
     )
+    costs_per_month = 30 * sum(
+        map(lambda x: x['total_price'], list(fuel_consumptions_last_three_months)[:-1])) / last_three_months_days
 
-
-@app.route('/car/<car_id>')
-def car(car_id):
-    if car_id in car_ids:
-        car_properties = get_car_properties_from(car_id)
-        fuel_consumptions = add_total_price_to(
-            add_average_consumption_to(get_chronological_fuel_consumptions_from(car_id)))
-        total_amount = sum(map(lambda x: x['amount'], fuel_consumptions))
-        total_distance = sum(map(lambda x: x['distance'], fuel_consumptions))
-        total_average_consumption = round(100 * total_amount / total_distance, 2)
-        fuel_consumptions_last_year, last_year_days = get_all_fuel_consumptions_and_days_within_last(365,
-                                                                                                     fuel_consumptions)
-        distance_per_year = 365 * sum(
-            map(lambda x: x['distance'], list(fuel_consumptions_last_year)[:-1])) / last_year_days
-        fuel_consumptions_last_three_months, last_three_months_days = get_all_fuel_consumptions_and_days_within_last(
-            90, fuel_consumptions
-        )
-        costs_per_month = 30 * sum(
-            map(lambda x: x['total_price'], list(fuel_consumptions_last_three_months)[:-1])) / last_three_months_days
-        # noinspection PyUnresolvedReferences
-        return render_template(
-            'car.html',
+    filename = os.path.join(root, '../docs', 'car_' + car_id + '.html')
+    with open(filename, 'w') as fh:
+        fh.write(car_template.render(
             car_name=car_properties["name"],
             fuel_consumptions=get_two_digits_formatted_fuel_consumptions(fuel_consumptions),
             total_average_consumption=total_average_consumption,
@@ -45,20 +47,18 @@ def car(car_id):
             costs_per_month=round(costs_per_month, 2),
             car_ids=sorted(car_ids),
             current_car_id=car_id
-        )
-    else:
-        return "car not found"
+        ))
 
 
 def get_chronological_fuel_consumptions_from(car_id):
-    file = open('data/' + car_id + '/fuel_consumptions.json')
+    file = open('../data/' + car_id + '/fuel_consumptions.json')
     fuel_consumptions = json.load(file)
     file.close()
     return sorted(fuel_consumptions, key=lambda x: datetime.strptime(x['date'], '%d.%m.%Y'), reverse=True)
 
 
 def get_car_properties_from(car_id):
-    file = open('data/' + car_id + '/car_properties.json')
+    file = open('../data/' + car_id + '/car_properties.json')
     car_properties = json.load(file)
     file.close()
     return car_properties
@@ -98,5 +98,9 @@ def get_two_digits_formatted_fuel_consumptions(fuel_consumptions):
 
 
 if __name__ == '__main__':
-    car_ids = os.listdir("data")
-    app.run(host='0.0.0.0', port=5000)
+    print("Start generating the files for pages")
+    car_ids = os.listdir("../data")
+    generate_index()
+    for car in car_ids:
+        generate_car(car)
+    print("Finish generating the files")
